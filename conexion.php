2fb36bd4404528616306bd4a243a72018860491d
Ariel F.G <arielg.incgarcia@gmail.com>
2022-03-31 15:44:27 -0700

    Probando conexion con github desde la rama productlist

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $conexion = mysqli_connect("b4c84aaea9zjwfysumkj-mysql.services.clever-cloud.com","uemutsqcjatwyjtq","vZnvBnvoaT8eDHjy73jH","b4c84aaea9zjwfysumkj");
+
+    if(!$conexion)
+    {
+        echo 'Error de conexion';
+        exit;
+    }
+
+?>
