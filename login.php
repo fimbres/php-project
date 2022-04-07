03a7fbe08d17cbbdef772fcc7522a5fb26d972d2
Alexis Patiño <patino.alexis@uabc.edu.mx>
2022-04-07 16:12:01 -0700

    Login error
    Se corrigió un error del login el cual no te podían agarrar los archivos css, ni el archivo js, También a la hora de iniciar sesión, con llenar los espacios te validaba el inicio de sesion.

--- login.php
+++ login.php
@@ -7,7 +7,11 @@
         <meta name="description" content="Simple Web Application For Products Management." />
         <meta name="author" content="472 UABC Group" />
         <title>Log in</title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){?>
+            <link href="../css/styles.css" rel="stylesheet" />
+        <?php } else{?>
+            <link href="css/styles.css" rel="stylesheet" />
+        <?php }?>
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="bg-light">
@@ -20,18 +24,23 @@
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header bg-white"><h3 class="text-center font-weight-light my-4">Login</h3></div>
                                     <div class="card-body">
-                                        <form action="php/validar_login.php" method="post">
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="Username" name ="Username" type="text" placeholder="Example69" />
-                                                <label for="Username">Username</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="Password" name ="Password" type="password" placeholder="Password" />
-                                                <label for="Password">Password</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <input class="btn btn-primary w-100" type="submit" value = "ingresar">    
-                                            </div>
+                                        <form <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){?>
+                                            action="../php/validar_login.php"
+                                            <?php } else{?>
+                                                action="php/validar_login.php"
+                                            <?php }?>
+                                            method="post">
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="Username" name ="Username" type="text" placeholder="Example69" />
+                                                    <label for="Username">Username</label>
+                                                </div>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="Password" name ="Password" type="password" placeholder="Password" />
+                                                    <label for="Password">Password</label>
+                                                </div>
+                                                <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                    <input class="btn btn-primary w-100" type="submit" value = "ingresar">    
+                                                </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center py-3">
@@ -45,6 +54,11 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){?>
+            <script src="../js/scripts.js"></script>
+        <?php } else{?>
+            <script src="js/scripts.js"></script>
+        <?php }?>
+        
     </body>
 </html>
--- php/validar_login.php
+++ php/validar_login.php
@@ -3,23 +3,22 @@
     $contraseña = $_POST['Password'];
     session_start();
 
-    if(empty($usuario) || empty($contraseña))
+    if(!empty($usuario) && !empty($contraseña))
     {
-        echo "<div class='alert alert-warning' role='alert'>Both fields are required.</div>";
-        include("../login.php");
-    }else{
         require 'conexion.php';
         $query = "SELECT * FROM  tb_usuarios where usuario = '$usuario' and password = '$contraseña'";
         $res = mysqli_query($conexion,$query);
-
+        $res = mysqli_fetch_array($res);
         if($res){
             $_SESSION['user'] = $usuario;
             header("location: ../index.php");
         }else{
-            echo "<div class='alert alert-danger' role='alert'>Authentication error!</div>";
+            echo "<div class='alert alert-danger' role='alert'>The user was not there!</div>";
             include("../login.php");
         }
-        mysqli_free_result($res);
         mysqli_close($conexion);
+    }else{
+        echo "<div class='alert alert-warning' role='alert'>Both fields are required.</div>";
+        include("../login.php");
     }
 ?>
